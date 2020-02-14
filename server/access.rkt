ab8139c23575a0e827b22239668e4f226168d5a3
Christian Fischer <christian@chfi.se>
2020-02-14 12:44:27 -0600

    Redesigned with "privilege lines" and access lattices

--- server/access.rkt
+++ server/access.rkt
@@ -11,10 +11,53 @@
 (define (mk-privilege g f)
   (privilege g f))
 
-(define (perform-action priv u args)
-  (if (has-user? (privilege-group priv) u)
-      (privilege-action args)
-      'no-access))
+
+; A `privilege-line` is a set of privileges that have a monotonically
+; increasing structure, e.g. no access <- can read <- can read & write
+;; (struct privilege-line (actions))
+(define (privilege-line name actions)
+  '(name . actions))
+
+
+(define (pl-dataset-data dset)
+  (define name 'data)
+  (define edit (cons 'edit
+                     (lambda (f)
+                       (set-dataset-data! dset (f (dataset-data dset))))))
+  (define view (cons 'view
+                     (lambda () (dataset-data dset))))
+  (define no-access (cons 'no-access
+                          (lambda () 'no-access)))
+  (cons name '(edit view no-access)))
+
+
+(define (pl-dataset-desc dset)
+  (define name 'desc)
+  (define edit (cons 'edit
+                     (lambda (f)
+                       (set-dataset-desc! dset (f (dataset-desc dset))))))
+  (define view (cons 'view
+                     (lambda () (dataset-desc dset))))
+  (define no-access (cons 'no-access
+                          (lambda () 'no-access)))
+  (cons name '(edit view no-access)))
+
+(define (combine-lines ls)
+  (apply list ls))
+
+(define (default-access pls)
+  (dict-map pls
+            (lambda (k v)
+              (if (and (dict? pls) (list? pls))
+                  (cons k (car (last v)))
+                  (error 'not-privilege-line)))))
+
+
+;; (define (pl-dataset-data dset)
+;;   (privilege-line 'data '('edit-data
+
+;; (define (privilege-poset lines))
+
 
 ; Groups and users are the second, for now, a user is simply an ID
 (struct user (id))
@@ -26,8 +69,12 @@
 ; non-empty set of admin users
 (struct group (admins members))
 
+(define (all-members g)
+  (set->list (set-union (group-admins g)
+                        (group-members g))))
+
 (define (mk-group owner)
-  (group (set owner) (set empty)))
+  (group (set owner) (list->set empty)))
 
 (define (has-user? g u)
   (or (set-member? (group-admins g) u)
@@ -60,30 +107,31 @@
 ; ID, etc.)
 (struct resource (name owner content privileges))
 
-(define (try-action res user action args)
+(define (try-action res user action . args)
   (define priv (hash-ref (resource-privileges res)
                          action
                          (lambda () (raise 'action-not-found))))
-  ;; priv)
   (define grp (privilege-group priv))
-  ;; grp)
-  (if (has-user? grp user)
+  (if (or (has-user? grp user)
+          (equal? user (resource-owner res)))
       (apply (privilege-action priv) args)
       (error 'no-access)))
 
 ; Return a list of the privileges, and their respective user groups,
 ; for a resource
-;; (define (resource-list-privileges res)
-;;   (
+(define (resource-list-privileges res)
+  (hash-map (resource-privileges res)
+            (lambda (k p)
+              (cons k
+                    (map user-id
+                         (all-members (privilege-group p)))))))
+
+;; (define (admin-privilege group
+
 
 (struct dataset (desc data) #:mutable)
 
 (struct collection (metadata datasets) #:mutable)
-
-;; (define (mk-data data)
-
-;; (define (view-data data)
-;;   (lambda () (data)))
 
 (define (mk-dataset name data desc owner)
   (define group (mk-group owner))
@@ -98,11 +146,19 @@
                              (set-dataset-data! dset (f (dataset-data dset))))))
   (hash-set! privs 'view-desc
              (mk-privilege group
-                           'undefined))
+                           (lambda () (dataset-desc dset))))
   (hash-set! privs 'edit-desc
              (mk-privilege group
-                           'undefined))
+                           (lambda (f)
+                             (set-dataset-desc! dset (f (dataset-desc dset))))))
+  ;; (hash-set! privs 'edit-privileges
+  ;;            (mk-privilege group
+  ;;                          (lambda (f)
+  ;;                            (
   (resource name owner dset privs))
+
+
+
 
 ;; (define (mk-resource n o ps)
 ;;   (resource n o ps))
