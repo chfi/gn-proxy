18c03e87c2fc38a8927ae4ea47cd2bde3d7c8c0f
Christian Fischer <christian@chfi.se>
2020-02-18 15:57:43 -0600

    Calculate access masks based on user group membership

--- server/access.rkt
+++ server/access.rkt
@@ -2,16 +2,6 @@
 
 (require racket/set)
 
-
-; The `privilege` is one of the two fundamental building blocks, and
-; is a group of users together with an `action`, which is a function
-; that can be called by the privileged users.
-(struct privilege (group action))
-
-(define (mk-privilege g f)
-  (privilege g f))
-
-
 ; A `privilege-line` is a set of privileges that have a monotonically
 ; increasing structure, e.g. no access <- can read <- can read & write
 ;; (struct privilege-line (actions))
@@ -28,7 +18,7 @@
                      (lambda () (dataset-data dset))))
   (define no-access (cons 'no-access
                           (lambda () 'no-access)))
-  (cons name '(edit view no-access)))
+  (cons name (list no-access view edit)))
 
 
 (define (pl-dataset-desc dset)
@@ -40,41 +30,162 @@
                      (lambda () (dataset-desc dset))))
   (define no-access (cons 'no-access
                           (lambda () 'no-access)))
-  (cons name '(edit view no-access)))
+  (cons name (list no-access view edit)))
 
-(define (combine-lines ls)
-  (apply list ls))
+(define (pl-resource-admin resource)
+  (define name 'admin)
+  (define edit-admins (cons 'edit-admins
+                            'undefined))
+  (define edit-access (cons 'edit-access
+                            'undefined))
+  (define not-admin (cons 'not-admin
+                          'undefined))
+  (cons name (list not-admin edit-access edit-admins)))
 
-(define (default-access pls)
+(struct resource (name owner content plines group-masks))
+
+(define plines
+  (list (pl-dataset-data empty)
+        (pl-dataset-desc "test desc")
+        (pl-resource-admin empty)))
+
+
+(define (minimum-access-mask pls)
+  (dict-map pls
+            (lambda (k v)
+              (if (and (dict? pls) (list? pls))
+                  (cons k (caar v))
+                  (error 'not-privilege-line)))))
+
+(define (maximum-access-mask pls)
   (dict-map pls
             (lambda (k v)
               (if (and (dict? pls) (list? pls))
                   (cons k (car (last v)))
                   (error 'not-privilege-line)))))
 
+(define tst-res
+  (let ([plines (list (pl-dataset-data empty)
+                      (pl-dataset-desc "test desc")
+                      (pl-resource-admin empty))])
+    (resource "test"
+               (user-by-id 1)
+               empty
+               plines
+               (list (cons 1 (maximum-access-mask plines))
+                     (cons 2 (minimum-access-mask plines))
+                     (cons 0 (list '(data . edit)
+                                   '(desc . no-access)
+                                   '(admin . not-admin)))))))
 
-;; (define (pl-dataset-data dset)
-;;   (privilege-line 'data '('edit-data
 
-;; (define (privilege-poset lines))
+
+; Given a plines poset and a list of masks, return a mask with the
+; highest access level per pline that was found in the masks.
+(define (mask-join plines . masks)
+  (define (mask-index pline)
+    (lambda (access-level)
+      (index-of pline access-level
+                (lambda (x y) (eq? (car x) y)))))
+  (dict-map plines
+            (lambda (k v)
+              (cons k (argmax (mask-index v)
+                              (map (curryr dict-ref k) masks))))))
+
+; Given a resource and a user, get the masks for that user based
+; on the per-group masks in the resource privileges.
+(define (user-masks res u)
+  (define masks (map (curry dict-ref (resource-group-masks res))
+                     (map group-id (groups-by-user u))))
+  ;; masks)
+  (if (empty? masks)
+      (list (minimum-access-mask (resource-plines res)))
+      masks))
+
+; Given a resource and a mask, calculate the actual mask in case the
+; mask has admin privileges. Admins have the maximum access privileges,
+; except their admin privileges may be limited.
+(define (admin-mask res m)
+  (define admin-level (dict-ref m 'admin))
+  (if (eq? admin-level 'not-admin)
+      m
+      (dict-set (maximum-access-mask (resource-plines res))
+                'admin
+                admin-level)))
+
+; The owner of a resource has complete access.
+(define (owner-mask res)
+  (maximum-access-mask (resource-plines res)))
+
+; Given a resource and a user, calculate the user's canonical access mask
+; based on the user's group membership, whether or not they're an admin,
+; and whether or not they're the resource owner.
+(define (user-canonical-mask res u)
+  (if (eq? u (resource-owner res))
+      (owner-mask res)
+      (admin-mask res
+                  (apply mask-join
+                         (resource-plines res)
+                         (user-masks res u)))))
 
 
 ; Groups and users are the second, for now, a user is simply an ID
-(struct user (id))
+(struct user (id name))
+
+; Placeholder global user "DB" with some entries for testing
+(define user-db
+  (list (user 1 "admin1")
+        (user 2 "user1")
+        (user 3 "user2")
+        (user 4 "admin2")))
+
+(define (user-by-id id)
+  (findf (lambda (u) (eq? id (user-id u))) user-db))
 
 (define (same-user? a b)
   (equal? (user-id a) (user-id b)))
 
 ;A `group` is a collection of users of different levels, including a
 ; non-empty set of admin users
-(struct group (admins members))
+(struct group (id admins members))
 
+; Placeholder global group "DB", will be replaced by an actual DB
+; system etc.
+(define group-db
+  (mutable-set))
+
+
+(set-add! group-db (group 0
+                          (set (user-by-id 1))
+                          (set (user-by-id 3))))
+
+(define (group-by-id id)
+  (findf (lambda (g) (eq? id (group-id g)))
+         (set->list group-db)))
+
+(define (groups-by-user u)
+  (filter (lambda (g) (has-user? g u))
+          (set->list group-db)))
+
+;
 (define (all-members g)
   (set->list (set-union (group-admins g)
                         (group-members g))))
 
-(define (mk-group owner)
-  (group (set owner) (list->set empty)))
+;; (define (mk-group id owner)
+;;   (group (set owner) (list->set empty)))
+
+; Placeholder function for adding a group to the global DB; only works
+; if the given user ID exists in the user Db
+(define (add-group-for-owner owner-id)
+  (define user (user-by-id owner-id))
+  (when user
+    (set-add! group-db (group (set-count group-db)
+                              (set user)
+                              (set)))))
+
+(add-group-for-owner 4)
+(add-group-for-owner 3)
 
 (define (has-user? g u)
   (or (set-member? (group-admins g) u)
@@ -105,26 +216,26 @@
 ; Resources are named collections of privileges with an owner, and the
 ; contents that are used by the privilege actions (e.g. URL, dataset
 ; ID, etc.)
-(struct resource (name owner content privileges))
+;; (struct resource (name owner content privileges))
 
-(define (try-action res user action . args)
-  (define priv (hash-ref (resource-privileges res)
-                         action
-                         (lambda () (raise 'action-not-found))))
-  (define grp (privilege-group priv))
-  (if (or (has-user? grp user)
-          (equal? user (resource-owner res)))
-      (apply (privilege-action priv) args)
-      (error 'no-access)))
+;; (define (try-action res user action . args)
+;;   (define priv (hash-ref (resource-privileges res)
+;;                          action
+;;                          (lambda () (raise 'action-not-found))))
+;;   (define grp (privilege-group priv))
+;;   (if (or (has-user? grp user)
+;;           (equal? user (resource-owner res)))
+;;       (apply (privilege-action priv) args)
+;;       (error 'no-access)))
 
 ; Return a list of the privileges, and their respective user groups,
 ; for a resource
-(define (resource-list-privileges res)
-  (hash-map (resource-privileges res)
-            (lambda (k p)
-              (cons k
-                    (map user-id
-                         (all-members (privilege-group p)))))))
+;; (define (resource-list-privileges res)
+;;   (hash-map (resource-privileges res)
+;;             (lambda (k p)
+;;               (cons k
+;;                     (map user-id
+;;                          (all-members (privilege-group p)))))))
 
 ;; (define (admin-privilege group
 
@@ -133,29 +244,30 @@
 
 (struct collection (metadata datasets) #:mutable)
 
-(define (mk-dataset name data desc owner)
-  (define group (mk-group owner))
-  (define privs (make-hash))
-  (define dset (dataset desc data))
-  (hash-set! privs 'view-data
-             (mk-privilege group
-                           (lambda () (dataset-data dset))))
-  (hash-set! privs 'edit-data
-             (mk-privilege group
-                           (lambda (f)
-                             (set-dataset-data! dset (f (dataset-data dset))))))
-  (hash-set! privs 'view-desc
-             (mk-privilege group
-                           (lambda () (dataset-desc dset))))
-  (hash-set! privs 'edit-desc
-             (mk-privilege group
-                           (lambda (f)
-                             (set-dataset-desc! dset (f (dataset-desc dset))))))
-  ;; (hash-set! privs 'edit-privileges
-  ;;            (mk-privilege group
-  ;;                          (lambda (f)
-  ;;                            (
-  (resource name owner dset privs))
+
+;; (define (mk-dataset name data desc owner)
+;;   (define group (mk-group owner))
+;;   (define privs (make-hash))
+;;   (define dset (dataset desc data))
+;;   (hash-set! privs 'view-data
+;;              (mk-privilege group
+;;                            (lambda () (dataset-data dset))))
+;;   (hash-set! privs 'edit-data
+;;              (mk-privilege group
+;;                            (lambda (f)
+;;                              (set-dataset-data! dset (f (dataset-data dset))))))
+;;   (hash-set! privs 'view-desc
+;;              (mk-privilege group
+;;                            (lambda () (dataset-desc dset))))
+;;   (hash-set! privs 'edit-desc
+;;              (mk-privilege group
+;;                            (lambda (f)
+;;                              (set-dataset-desc! dset (f (dataset-desc dset))))))
+;;   ;; (hash-set! privs 'edit-privileges
+;;   ;;            (mk-privilege group
+;;   ;;                          (lambda (f)
+;;   ;;                            (
+;;   (resource name owner dset privs))
 
 
 
